22597a39fd475d02690002418df886578381f867
Tai Bui <taibui97@outlook.com>
2026-02-17 00:00:19 -0800

    create layout for auth paths

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+export default function AuthLayout({
+    children,
+}: {
+    children: ReactNode;
+}) {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-muted/40 to-muted p-6">
+
+            <div className="w-full max-w-md">
+                {children}
+            </div>
+
+        </div>
+    );
+}
+
