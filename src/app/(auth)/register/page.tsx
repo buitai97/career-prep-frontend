68a3cfb4f7064c5d1a708b89f5b14f80d8cd65e5
Tai Bui <taibui97@outlook.com>
2026-02-18 01:50:16 -0800

    config eslint

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -8,7 +8,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Card, CardAction, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
 import { registerUser } from "@/app/(auth)/api";
 
